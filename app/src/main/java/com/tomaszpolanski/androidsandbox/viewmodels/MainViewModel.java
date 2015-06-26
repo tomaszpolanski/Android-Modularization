885e5e27edb2e2189b664fa5cadb213f9e2b8c08
tomaszpolanski <krigerek@gmail.com>
2015-06-26 08:46:31 +0200

    Cleaning up

--- app/src/main/java/com/tomaszpolanski/androidsandbox/viewmodels/MainViewModel.java
+++ app/src/main/java/com/tomaszpolanski/androidsandbox/viewmodels/MainViewModel.java
@@ -3,94 +3,28 @@ package com.tomaszpolanski.androidsandbox.viewmodels;
 import android.support.annotation.NonNull;
 import android.util.Log;
 
-import java.util.ArrayList;
+import java.util.List;
 
-import java8.util.function.Consumer;
-import java8.util.function.IntSupplier;
-import java8.util.function.IntUnaryOperator;
-import java8.util.function.Supplier;
+import java8.util.stream.Collectors;
 import java8.util.stream.IntStreams;
 import java8.util.stream.Stream;
-import java8.util.stream.StreamSupport;
 
 public class MainViewModel extends BaseViewModel {
 
 
     public MainViewModel() {
 
-//        final Observable<Integer> numbers = Observable.just(1);
-//
-//        subscribe(numbers.subscribe(number -> Log.e("Subscription", "Sub1: " + number)));
+        toList();
+    }
 
-        //////////////
-
-//        final Observable<Integer> numbers = Observable.just(1)
-//                                                      .map(number -> expensiveOperation(number));
-//
-//        subscribe(numbers.subscribe(number -> Log.e("Subscription", "Sub1: " + number)));
-//
-
-        //////////////
-
-//        final Observable<Integer> numbers = Observable.just(1)
-//                                                      .map(number -> expensiveOperation(number));
-//
-//        subscribe(numbers.subscribe(number -> Log.e("Subscription", "Sub1: " + number)));
-//        subscribe(numbers.subscribe(number -> Log.e("Subscription", "Sub2: " + number)));
-
-        ///////////////
-        //Cold
-//        final ConnectableObservable<Integer> numbers = Observable.just(1)
-//                                                                 .map(number -> expensiveOperation(number))
-//                                                                 .publish();
-//
-//        subscribe(numbers.subscribe(number -> Log.e("Subscription", "Sub1: " + number)));
-//        subscribe(numbers.subscribe(number -> Log.e("Subscription", "Sub2: " + number)));
-//
-//        subscribe(numbers.connect());
-
-        ///////////
-        // Hot
-//        final PublishSubject<Integer> hot = PublishSubject.create();
-//        final Observable<Integer> numbers = hot
-//                .map(number -> expensiveOperation(number))
-//                .share();
-//
-//
-//
-//        subscribe(numbers.subscribe(number -> Log.e("Subscription", "Sub1: " + number)));
-//        subscribe(numbers.subscribe(number -> Log.e("Subscription", "Sub2: " + number)));
-//        hot.onNext(2);
-//        hot.onNext(4);
-        //////////
-        // Timer
-//        final Observable<Integer> numbers = Observable.timer(2, 2, TimeUnit.SECONDS).map(lon -> lon.intValue())
-//                                                                 .map(number -> expensiveOperation(number));
-//                                                                //.share();
-//
-//        subscribe(numbers.subscribe(number -> Log.e("Subscription", "Sub1: " + number)));
-//        subscribe(numbers.subscribe(number -> Log.e("Subscription", "Sub2: " + number)));
-
-        ///////////
-        // Why not cold like this?
-
-//        final Observable<Integer> n = Observable.create((Subscriber<? super Integer> subscriber) -> {
-//            subscriber.onNext(10);
-//          //  subscriber.onCompleted();
-//        });
-//
-//        final Observable<Integer> numbers = n
-//                .map(number -> expensiveOperation(number))
-//                .share();
-//
-//
-//        subscribe(numbers.subscribe(number -> Log.e("Subscription", "Sub1: " + number)));
-//        subscribe(numbers.subscribe(number -> Log.e("Subscription", "Sub2: " + number)));
-
-        //
-
+    private static void iterateStream() {
         final Stream<String> st = getRangedStream();
-        st.forEach(s -> Log.e("QQQ", s));
+        st.forEach(s -> Log.e("Print", s));
+    }
+
+    private static void toList() {
+        final Stream<String> st = getRangedStream();
+        final List<String> str = st.collect(Collectors.toList());
     }
 
     @NonNull
@@ -99,11 +33,5 @@ public class MainViewModel extends BaseViewModel {
                 .limit(10)
                 .mapToObj(i -> "" + i);
     }
-
-    private static int expensiveOperation(final int number) {
-        Log.e("MainViewModel", "Expensive");
-        return number;
-    }
-
 
 }
