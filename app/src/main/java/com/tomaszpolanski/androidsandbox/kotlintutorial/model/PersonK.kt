b908f399ee0144dd858941447d60d22101b83791
tomaszpolanski <krigerek@gmail.com>
2015-07-17 16:07:26 +0200

    Minor kotlin refactoring

--- app/src/main/java/com/tomaszpolanski/androidsandbox/kotlintutorial/model/PersonK.kt
+++ app/src/main/java/com/tomaszpolanski/androidsandbox/kotlintutorial/model/PersonK.kt
@@ -1,3 +1,6 @@
 package com.tomaszpolanski.androidsandbox.kotlintutorial.model
 
-public data class PersonK(val name : String, val lastName : String, val age: Int, val job: String?)
+public data class PersonK(val name : String,
+                          val lastName : String,
+                          val age: Int,
+                          val job: String?)
